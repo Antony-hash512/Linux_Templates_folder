6f5b73192ff492a9a277a788c7dbfc2d2b98821b
Antony-hash512 <antony.ictr@gmail.com>
2025-05-24 13:19:34 +0400

    fix

--- miniUtils/zst_repacker.sh
+++ miniUtils/zst_repacker.sh
@@ -31,7 +31,8 @@ TEMP_DIR=$(mktemp -d)
 tar --no-same-owner -xvf "$INPUT_ARCH" -C "$TEMP_DIR"
 
 #если версия tar больше 1.31, то перепаковываем архив в zstd напрямую без внешнего компрессора
-if [ $(tar --version | grep -oP 'tar \(GNU tar\) \K[\d.]+') -gt 1.31 ]; then
+TAR_VERSION=$(tar --version | grep -oP 'tar \(GNU tar\) \K[\d.]+')
+if [ "$(printf '%s\n' "$TAR_VERSION" "1.31" | sort -V | tail -n1)" = "$TAR_VERSION" ] && [ "$TAR_VERSION" != "1.31" ]; then
     tar --zstd --numeric-owner -cvf "${INPUT_ARCH_BASENAME}.tar.zst" -C "$TEMP_DIR" .
 else
     tar -I zstd --numeric-owner -cvf "${INPUT_ARCH_BASENAME}.tar.zst" -C "$TEMP_DIR" .
