617e0dc4234c66702a9143a1dda422d09d4dec7e
Antony-hash512 <antony.ictr@gmail.com>
2025-05-24 13:41:37 +0400

    fix

--- miniUtils/zst_repacker.sh
+++ miniUtils/zst_repacker.sh
@@ -32,9 +32,15 @@ tar --no-same-owner -xvf "$INPUT_ARCH" -C "$TEMP_DIR"
 
 #если версия tar больше 1.31, то перепаковываем архив в zstd напрямую без внешнего компрессора
 TAR_VERSION=$(tar --version | grep -oP 'tar \(GNU tar\) \K[\d.]+')
-if [ "$(printf '%s\n' "$TAR_VERSION" "1.31" | sort -V | tail -n1)" = "$TAR_VERSION" ] && [ "$TAR_VERSION" != "1.31" ]; then
+# Обрезаем версию до major.minor (убираем всё после второй точки)
+TAR_VERSION=$(echo "$TAR_VERSION" | grep -oP '^\d+\.\d+')
+echo "Tar version: $TAR_VERSION"
+# Сравниваем версии как числа с плавающей точкой через bc
+if [ "$(echo "$TAR_VERSION > 1.31" | bc)" -eq 1 ]; then
+    echo "using directly compression"
     tar --zstd --numeric-owner -cvf "${INPUT_ARCH_BASENAME}.tar.zst" -C "$TEMP_DIR" .
 else
+    echo "using an external compressor"
     tar -I zstd --numeric-owner -cvf "${INPUT_ARCH_BASENAME}.tar.zst" -C "$TEMP_DIR" .
 fi
 
